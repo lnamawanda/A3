1a01c6fa4412b93026626ad1efd9fd526dbc4e07
lnamawanda <lnamawanda@student.unimelb.edu.au>
2026-05-02 19:06:43 +1000

    Organising fonts and CSS layers

--- info.js
+++ info.js
@@ -1,2 +1,17 @@
 console.log ("hello word");
 
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+let menuOpen = false;
+
+hamburger.addEventListener('click', () => {
+    if (menuOpen == false){
+        navLinks.style.display = "block";
+        menuOpen = true;
+    }
+    else if (menuOpen == true){
+        navLinks.style.display = "none";
+        menuOpen = false;
+
+    }
+});
